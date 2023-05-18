8ca79153481b01199f800d6d5acf8e2c149169d9
Gary <jj3179@columbia.edu>
2023-05-18 19:12:54 -0400

    [WIP] Bug Caught

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import Navbar from "./components/Navbar";
+import HomeScreen from "./components/screens/HomeScreen";
+import LoginScreen from "./components/screens/LoginScreen";
+import RegisterScreen from "./components/screens/RegisterScreen";
 
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings()), []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="APP">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<HomeScreen />} />
+          <Route exact path="/login" element={<LoginScreen />} />
+          <Route exact path="/register" element={<RegisterScreen />} />
+        </Routes>
+      </ThemeProvider>
     </div>
   );
 }
