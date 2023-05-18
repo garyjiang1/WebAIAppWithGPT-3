e7a8cca57bffe755060672005e69753bf659dda3
Gary <jj3179@columbia.edu>
2023-05-18 15:41:31 -0400

    [DONE] connected to db

--- server.js
+++ server.js
@@ -12,6 +12,19 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+mongoose.set('strictQuery', true);
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    throw err;
+  });
+  
 const port = process.env.PORT || 4242;
 
 app.listen(port, () => { console.log(`Server is running on port ${port}`)});
