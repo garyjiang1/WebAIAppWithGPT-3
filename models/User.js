3f850927a9dff2dad795780c6bd948ed5aa093c7
Gary <jj3179@columbia.edu>
2023-05-18 16:20:47 -0400

    [DONE] Error Handle
    added error handling for user creation in the database

---
+++ models/User.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bycrypt.js");
+
+const UsercSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: [true, "Please provide a username"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+    match: [
+      /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+      "Please provide a valid email",
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minLength: [6, "Password must be at least 6 characters long"],
+    select: false,
+  },
+  custmoerId: {
+    type: String,
+    default: "",
+  },
+  subscription: {
+    type: String,
+    default: "",
+  },
+});
+
+// hashing the password before saving to db
+UsercSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+//match passwords
+UsercSchema.methods.matchPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+//sign JWT and return
+UsercSchema.methods.getSignedJwtToken = function () {
+  const accessToken = jwt.sign(
+    { id: this._id },
+    process.env.JWT_ACCESS_SECRET,
+    { expiresIn: process.env.JWT_EXPIRE }
+  );
+  const refreshToken = jwt.sign(
+    { id: this._id },
+    process.env.JWT_REFRESH_SECRET,
+    { expiresIn: process_env.JWT_REFRESH_EXPIRE }
+  );
+  res.cookie("refreshToken", `${refreshToken}`, {
+    maxAge: 86400 * 7000,
+    httpOnly: true,
+  });
+  return { accessToken, refreshToken };
+};
+
+const user = mongoose.model("User", UsercSchema);
+
+module.exports = user;
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 // connecting to the config.env
-require('dotenv').config({path: './config.env'});
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+require("dotenv").config({ path: "./config.env" });
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
-
-// connecting to DB
-const mongoose = require ('mongoose');
+const mongoose = require("mongoose");
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
 
-mongoose.set('strictQuery', true);
+// connecting to DB
+mongoose.set("strictQuery", true);
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -24,7 +24,9 @@ mongoose
   .catch((err) => {
     throw err;
   });
-  
+
 const port = process.env.PORT || 4242;
 
-app.listen(port, () => { console.log(`Server is running on port ${port}`)});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
