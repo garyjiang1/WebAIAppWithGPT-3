4f0d6bd48e9c36f79dad1536ea37bdfc007b4c4f
Gary <jj3179@columbia.edu>
2023-05-18 12:50:50 -0400

    initialize
    update project progress

---
+++ server.js
@@ -0,0 +1,17 @@
+// connecting to the config.env
+require('dotenv').config({path: './config.env'});
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+// connecting to DB
+const mongoose = require ('mongoose');
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+const port = process.env.PORT || 4242;
+
+app.listen(port, () => { console.log(`Server is running on port ${port}`)});
